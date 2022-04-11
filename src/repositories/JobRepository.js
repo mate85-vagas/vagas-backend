fb2d774f1a6bbb1105d209e501c565e7c16485f3
Bruno <brunodipaolo13@gmail.com>
2022-04-11 13:23:39 -0300

    Add: adiciona funcao de enviar email caso o usuario aplique para uma vaga

--- src/repositories/JobRepository.js
+++ src/repositories/JobRepository.js
@@ -1,6 +1,9 @@
 import Job from '../models/JobModel.js';
 import User_JobRepository from '../repositories/User_JobRepository.js';
+import UserRepository from './UserRepository.js';
 import { JobAttrs } from '../models/JobAttrs.js';
+import { mail_sender } from '../utils/emailSender.js';
+import ProfileRepository from './ProfileRepository.js';
 
 const getAllJobs = async (filters, itemsPerPage, pageNumber) => {
   const jobs = await Job.findAndCountAll({
@@ -43,6 +46,16 @@ const deleteJob = async (id) => {
 
 const applyToJob = async (userId, jobId) => {
   await User_JobRepository.createUser_Job(userId, jobId, false);
+  const userApplier = await UserRepository.getUserById(userId)
+  const infoUserReceiver = await User_JobRepository.getInformationByJobId(
+    jobId
+  )
+  const jobToApply = await getJobById(jobId)
+  const userReceiver = await UserRepository.getUserById(
+    infoUserReceiver.dataValues.userId
+  )
+  const profileUserApplier = await ProfileRepository.getProfileByUserId(userId)
+  await mail_sender(userApplier, userReceiver, profileUserApplier, jobToApply)
 };
 
 export default { getAllJobs, getJobById, updateJob, deleteJob, createJob, applyToJob };
