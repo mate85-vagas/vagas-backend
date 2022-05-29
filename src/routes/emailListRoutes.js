9651503dbb7b2bb3c3c55668258631850b8cbc94
Yuri Mello <yurelvis123@gmail.com>
2022-05-29 12:26:59 -0300

    Fix: route não utilizado

--- src/routes/emailListRoutes.js
+++ src/routes/emailListRoutes.js
@@ -7,7 +7,6 @@ const router = express.Router();
 //Routes for Skill
 router.get('/', getAllEmailLists);
 router.get('/:id', getEmailListById);
-router.post('/', createBulkSkills);
 router.patch('/:id', updateEmailList);
 router.patch('/update-all', updateAllIsActive);
 router.delete('/:id', deleteEmailList);
