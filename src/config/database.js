7cf33e150f183d5272c3cfb37f12311f0ca84073
Natan Moura <natloc215@hotmail.com>
2022-06-12 22:08:47 -0300

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -3,12 +3,12 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const db = new Sequelize(process.env.CLEARDB_DATABASE_URL);
+//const db = new Sequelize(process.env.CLEARDB_DATABASE_URL);
 
-/*const db = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USER, process.env.DATABASE_PASSWORD, {
+const db = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USER, process.env.DATABASE_PASSWORD, {
   host: process.env.DATABASE_HOST,
   dialect: 'mysql',
   port: '3306'
-});*/
+});
 
 export default db;
